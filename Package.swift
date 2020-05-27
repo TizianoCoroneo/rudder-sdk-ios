7cdf711af0d86e3d53eea7bbc6a4a7345523fadb
Tiziano Coroneo <tiziano@ticketswap.com>
2020-05-27 19:31:30 +0200

    Cleanup

--- Package.swift
+++ Package.swift
@@ -4,35 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "rudder-sdk-ios",
+    name: "Rudder",
     platforms: [
         .iOS(.v11),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "rudder-sdk-ios",
-            targets: ["rudder-sdk-ios"]),
+            name: "Rudder",
+            targets: ["Rudder"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "rudder-sdk-ios",
+            name: "Rudder",
             dependencies: []
-//            ,
-//            cSettings: [
-//                .headerSearchPath("Sources/include"),
-//                .headerSearchPath("Sources/rudder-sdk-ios/Ecomm"),
-//                .headerSearchPath("Sources/rudder-sdk-ios/Ecomm/Events"),
-//        ]
         ),
         .testTarget(
-            name: "rudder-sdk-iosTests",
-            dependencies: ["rudder-sdk-ios"]),
+            name: "RudderTests",
+            dependencies: ["Rudder"]),
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import rudder_sdk_iosTests
-
-var tests = [XCTestCaseEntry]()
-tests += rudder_sdk_iosTests.allTests()
-XCTMain(tests)
