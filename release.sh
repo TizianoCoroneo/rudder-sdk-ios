085ad5cb793720f035523992c3019af879e81bb1
Tiziano Coroneo <tiziano@ticketswap.com>
2020-05-28 12:08:22 +0200

    Added script to regenerate symbolic links to headers file to release.sh

--- release.sh
+++ release.sh
@@ -2,6 +2,11 @@ read -p "Are you sure to release? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  # Remove all the symlinks in the SPM module include directory
+  cd ./Sources/RudderObjC/include
+  rm -f ./*.h
+  # Regenerate all the symlinks from the header files in the project, placing them in the include folder to make SPM able to find them
+  ln -s ../../**/*.h .
   git tag -a $1 -m "Release $1"
   git push origin $1
 fi
